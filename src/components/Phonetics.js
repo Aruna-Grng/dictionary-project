3c45074dc688c6c242d19c17b5575a55a170fbca
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-05-11 17:02:39 -0500

    Bug fixed (Phonetics comp.)

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetics(props) {
     return (
         <div>
-            <a href={props.soundPhonetics.audio} target="_blank">Listen</a>{" "}
+            <a href={props.soundPhonetics.audio} target="_blank" rel="noreferrer">Listen</a>{" "}
             <br />
             <br />
             {props.soundPhonetics.text}
