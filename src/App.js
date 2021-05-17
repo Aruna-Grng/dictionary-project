8002f6659336a89b02301cc4629650765608c058
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-05-17 00:13:38 -0500

    Fixing bug.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           <h2>Search for a word...</h2>
         </div>
           <Search defaultKeyword="bird" />
-          <p>Open source in <a href="https://github.com/Aruna-Grng/dictionary-project" target="_blank">Github</a> by Aruna Gurung. </p>
+          <p>Open source in <a href="https://github.com/Aruna-Grng/dictionary-project" target="_blank" rel="noreferrer">Github</a> by Aruna Gurung. </p>
       </div>
     </div>
   );
